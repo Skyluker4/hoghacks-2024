719d2c3ef6ff0418e2586d9d4bea7a2c6dfad74c
Luke Simmons <luke5083@live.com>
2024-04-13 00:10:14 -0500

    Initial api stuff

---
+++ Server/playpredict/position.py
@@ -0,0 +1,7 @@
+class Position:
+	def __init__(self, yard, down, distance, own_side, possessing_team):
+		self.yard = 50
+		self.down = 1
+		self.distance = 10
+		self.own_side = True
+		self.possessing_team = False
