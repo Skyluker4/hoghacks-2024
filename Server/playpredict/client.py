0bba77397030d9772cc94cecc662d170818849f7
Luke Simmons <luke5083@live.com>
2024-04-13 13:15:01 -0500

    Pragmatically do stuff

--- Server/playpredict/client.py
+++ Server/playpredict/client.py
@@ -10,4 +10,29 @@ def updatePlay():
 
 @client_bp.route("/")
 def index():
-    return render_template("dashboard.html")
+
+    play_table_data = []
+    for playnum in range(0, 4):
+        a = dict()
+        a['number'] = playnum
+
+        play_table_data.append(a)
+
+    prediction_table_data = []
+    for prediction in range(0, 4):
+        a = dict()
+        a["rank"] = prediction
+        a["play"] = "test"
+        a["formation"] = "test"
+
+        prediction_table_data.append(a)
+
+    stat_table_data = []
+    for stat in range(0, 4):
+        a = dict()
+        a["name"] = "test"
+        a["value"] = "test"
+
+        stat_table_data.append(a)
+
+    return render_template("dashboard.html", plays=play_table_data, predictions=prediction_table_data, stats=stat_table_data)
